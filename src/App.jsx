7db1e89c0804809919b5069e8436f73a7c92c44f
vonguyennhatduy0706@gmail.com <vonguyennhatduy7625@gmail.com>
2024-03-30 23:10:07 +0700

    app

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BaiTapThucHanhLayout } from './BaiTapLayoutComponent/BaiTapThucHanhLayout'
 import { BaiTapGlasses } from './GlassesApp/BaiTapGlasses'
 
 
